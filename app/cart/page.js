8a4a5ef670ec3887453f7e0f631135e7937ae80e
jang dongmin <jangdongmin@jangui-MacBookPro.local>
2023-11-02 23:44:33 +0900

    first commit

---
+++ app/cart/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+    let name = "Jang"
+    return (
+      <div>
+        <h1>장바구니입니다</h1>
+      </div>
+    )
+  }
+  
---
+++ app/cart/payment/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+    let name = "Jang"
+    return (
+      <div>
+        <h1>결제페이지입니다</h1>
+      </div>
+    )
+  }
+  
